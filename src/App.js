0176260bb57690ff564e91e1c90d2e79005ccfd6
Rafael Belenguer <RABEAR@upv.edu.es>
2024-01-07 20:01:18 +0100

    RBA-2 Inicializar el proyecto base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout/Layout'
+import NotFound from './components/NotFound/NotFound';
+import Home from './routes/Home';
+
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "/Home", element: <Home /> },
+    ]
+  }
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
