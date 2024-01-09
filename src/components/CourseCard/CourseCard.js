a2eecfc5e487a07166f2ce75a2378f971e0ddf84
Rafael Belenguer <RABEAR@upv.edu.es>
2024-01-09 20:48:42 +0100

    RBA-7 [COMPONENT] CourseCard

---
+++ src/components/CourseCard/CourseCard.js
@@ -0,0 +1,14 @@
+function CourseCard (props) {
+    return (
+        <div className="p-1 flex flex-col space-y-1 border-solid border-2 border-black max-w-80">
+            <p id="courseTitle" className="text-sm text-end mr-2 font-semibold">{props.courseName}</p>
+            <div className="flex flex-row flex-space-x-4 p-1 max-h-32">
+                <img className="basis-1/4 p-1 rounded-sm" src={props.courseImage} alt="course image"/>
+                <p className="basis-3/4 p-1 text-left self-center text-ellipsis ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies nunc id justo imperdiet dignissim.</p>
+            </div>
+            <a href={props.courseLink} className="text-xs text-end mr-2 text-blue-600 font-medium underline">Ver...</a>
+        </div>
+    );
+}
+
+export default CourseCard;
